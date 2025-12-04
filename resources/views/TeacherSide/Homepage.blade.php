2c20140477e65e96dbd88554ac518f288de46c7f
ylledolly <159888427+ylledolly@users.noreply.github.com>
2025-12-05 02:05:40 +0800

    ICONNNNNNNNN

--- resources/views/TeacherSide/Homepage.blade.php
+++ resources/views/TeacherSide/Homepage.blade.php
@@ -3340,8 +3340,8 @@
                 game_speak: "icon-game",
                 achievement: "icon-achievement",
                 enrollment: "icon-enrollment",
-                video: "icon-material", //dinagdag q
-                ppt: "icon-material" //dinagdag ko
+                video: "icon-video", //dinagdag q
+                ppt: "icon-ppt" //dinagdag ko
             }[type] || "icon-default";
         }
 
@@ -3385,7 +3385,7 @@
                         <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"></path>
                     </svg>
                 `,
-                
+
                 video: `
                     <svg class="bi bi-camera-video" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1z"></path>
