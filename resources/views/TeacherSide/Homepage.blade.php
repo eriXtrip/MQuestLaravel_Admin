b63850421268cc2d4d5bf09d0d95de759b22287e
ylledolly <159888427+ylledolly@users.noreply.github.com>
2025-12-05 02:01:03 +0800

    HAYAAASSS

--- resources/views/TeacherSide/Homepage.blade.php
+++ resources/views/TeacherSide/Homepage.blade.php
@@ -3355,7 +3355,7 @@
                 achievement: "badge-achievement",
                 enrollment: "badge-enrollment",
                 video: "badge-material", //dinagdag q
-                ppt: "bagde-material" // dinagdag q
+                ppt: "badge-material" // dinagdag q
             }[type] || "badge-default";
         }
 
@@ -3384,7 +3384,8 @@
                         <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z"></path>
                         <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"></path>
                     </svg>
-                `
+                `,
+                
                 video: `
                     <svg class="bi bi-camera-video" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1z"></path>
