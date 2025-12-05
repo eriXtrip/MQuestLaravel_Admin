3a3710d6c34f8062115f37ef9cb64df88327beec
ylledolly <159888427+ylledolly@users.noreply.github.com>
2025-12-05 17:14:20 +0800

    lesson item locked bagde

--- resources/views/AdminDashboard.blade.php
+++ resources/views/AdminDashboard.blade.php
@@ -2498,7 +2498,8 @@
                                     aria-expanded="false" aria-controls="content${id}">
                                     <div class="header-left">
                                         <h2>Lesson ${lesson.lesson_number || idx+1}
-                                            <span class="lock-badge" data-bs-toggle="tooltip" data-bss-tooltip title="This lesson cannot be edited"><svg class="bi bi-lock me-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+                                            <span class="lock-badge" data-bs-toggle="tooltip" data-bss-tooltip title="This lesson cannot be edited">
+                                            <svg class="bi bi-lock me-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
                                                 <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1"></path>
                                             </svg> Locked </span>
                                         </h2>
