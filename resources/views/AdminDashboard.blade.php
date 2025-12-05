8e957512dcd412b87e7aeae84560ad7557d80ba8
ylledolly <159888427+ylledolly@users.noreply.github.com>
2025-12-05 17:05:17 +0800

    changes sa lesson item and css

--- resources/views/AdminDashboard.blade.php
+++ resources/views/AdminDashboard.blade.php
@@ -2497,13 +2497,23 @@
                                     data-bs-toggle="collapse" data-bs-target="#content${id}"
                                     aria-expanded="false" aria-controls="content${id}">
                                     <div class="header-left">
-                                        <h2>Lesson ${lesson.lesson_number || idx+1}</h2>
+                                        <h2>Lesson ${lesson.lesson_number || idx+1}
+                                            <span class="lock-badge" data-bs-toggle="tooltip" data-bss-tooltip title="This lesson cannot be edited"><svg class="bi bi-lock me-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+                                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1"></path>
+                                            </svg> Locked </span>
+                                        </h2>
                                         <p>${title}</p>
                                     </div>
                                     <div class="header-right"><span>+</span></div>
                                 </div>
                                 <div id="content${id}" class="collapse">
                                     <div class="lesson-content">
+                                        <div class="locked-banner">
+                                            <svg class="bi bi-exclamation-triangle-fill me-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+                                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"></path>
+                                            </svg>
+                                            <span> This lesson is locked and cannot be edited or deleted. </span>
+                                        </div>
                                         <div class="part-1 mb-3">
                                             <div class="mb-2 part-intro">
                                                 <h6>Lesson Overview</h6>
