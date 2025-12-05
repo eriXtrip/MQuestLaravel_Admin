278ab90be6baa7782c3697d70aceb0ad0f4aced7
ylledolly <159888427+ylledolly@users.noreply.github.com>
2025-12-05 16:00:58 +0800

    changes sa btn

--- resources/views/AdminDashboard.blade.php
+++ resources/views/AdminDashboard.blade.php
@@ -1650,7 +1650,7 @@
             <div class="modal-content">
                 <div class="modal-body text-center p-4">
                     <div class="mb-3 text-danger">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" viewBox="0 0 16 16" class="bi bi-exclamation-triangle-fill text-success">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" viewBox="0 0 16 16" class="bi bi-exclamation-triangle-fill text-danger">
                             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                         </svg>
                     </div>
@@ -1660,10 +1660,10 @@
                         Please review everything carefully before publishing.
                     </p>
                     <div class="d-flex gap-2 justify-content-center">
-                        <button class="btn btn-secondary rounded-pill px-4 py-2" type="button" data-bs-dismiss="modal">
+                        <button class="btn btn-outline-danger rounded-pill px-4 py-2" type="button" data-bs-dismiss="modal">
                             Cancel
                         </button>
-                        <button class="btn btn-success rounded-pill px-4 py-2" type="button" id="confirmPublishBtn">
+                        <button class="btn btn-outline-success rounded-pill px-4 py-2" type="button" id="confirmPublishBtn">
                             Publish Lesson
                         </button>
                     </div>
