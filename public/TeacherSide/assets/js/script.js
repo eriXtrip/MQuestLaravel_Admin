5d7f632b81da08bdd6d94ee85527741c9dc055d9
ylledolly <159888427+ylledolly@users.noreply.github.com>
2025-12-09 00:31:24 +0800

    svg calendar

--- public/TeacherSide/assets/js/script.js
+++ public/TeacherSide/assets/js/script.js
@@ -1442,7 +1442,9 @@ function initSectionsManager(fetchedSections = [], fetchedPupils = []) {
                          <span>${section.section_name}</span> 
                     </div> -->
                     <div class="pupil-section-school-year mt-1 d-flex align-items-center gap-2">
-                        <svg class="bi bi-calendar" width="1em" height="1em" fill="currentColor"><use href="#icon-calendar"/></svg>
+                        <svg class="bi bi-calendar4" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"></path>
+                        </svg>
                         <span>${section.school_year}</span>
                     </div>
                 </div>
