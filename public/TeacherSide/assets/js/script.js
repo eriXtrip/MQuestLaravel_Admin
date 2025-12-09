d03cdef58e05dd32afde8a6070297b9489ce5c5e
ylledolly <159888427+ylledolly@users.noreply.github.com>
2025-12-10 00:33:54 +0800

    final

--- public/TeacherSide/assets/js/script.js
+++ public/TeacherSide/assets/js/script.js
@@ -1631,8 +1631,8 @@ function initSectionsManager(fetchedSections = [], fetchedPupils = []) {
 
                 // Inject Warning SVG dynamically
                 injectSvg(modalIconContainer, `
-                    <svg width="50" height="50" viewBox="0 0 24 24" fill="#e53935">
-                        <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/>
+                    <svg class="bi bi-exclamation-triangle-fill text-danger" xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="#e53935" viewBox="0 0 16 16">
+                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"></path>
                     </svg>
                 `);
 
@@ -1649,8 +1649,8 @@ function initSectionsManager(fetchedSections = [], fetchedPupils = []) {
 
             // Inject Yellow Warning SVG
             injectSvg(modalIconContainer, `
-                <svg width="50" height="50" viewBox="0 0 24 24" fill="#FFA726">
-                    <path d="M1 21h22L12 2 1 21zm12 14h-2v-2h2v2zm0-4h-2v-4h2v4z"/>
+                <svg class="bi bi-exclamation-triangle-fill text-warning" xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="#FFA726" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"></path>
                 </svg>
             `);
 
